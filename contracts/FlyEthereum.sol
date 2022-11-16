fe76ee63dbb4270cd514af42c26dc4e133fc19f7
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-11-16 11:33:23 -0700

    add openzeppelin features to token

--- contracts/FlyEthereum.sol
+++ contracts/FlyEthereum.sol
@@ -1,62 +1,47 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-
-// constructor() payable ERC20("Vault Challenge Token", "VCT") {
-//         require(msg.value == 1 ether, "Must init the contract with 1 eth");
-//         _deposit(msg.sender, address(this), msg.value);
-//     }
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
 
-/// @custom:security-contact tikisailor@protonmail.com
-contract FlyEthereum is ERC4626{
+contract FlyEthereum is ERC4626, ERC20Burnable, Pausable, AccessControl, ERC20Permit {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(
         ERC4626 underlying_
-
-    ) ERC4626(underlying_) ERC20("FlyEthereum", "flyETH") {
-        // require(msg.value == 1 ether, "Must init the contract with 1 eth");
-        // _deposit(msg.sender, address(this), msg.value, msg.value);
-        // _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        // _grantRole(PAUSER_ROLE, msg.sender);
-        // _grantRole(MINTER_ROLE, msg.sender);
+    ) ERC4626(underlying_) ERC20("FlyEthereum", "flyETH") ERC20Permit("FlyEthereum") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
     }
+
 
     function decimals() public pure override(ERC20) returns (uint8) {
         return 18;
     }
 
-    // function pause() public onlyRole(PAUSER_ROLE) {
-    //     _pause();
-    // }
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
 
-    // function unpause() public onlyRole(PAUSER_ROLE) {
-    //     _unpause();
-    // }
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
 
-    // function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-    //     _mint(to, amount);
-    // }
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
 
-    // function _beforeTokenTransfer(address from, address to, uint256 amount)
-    //     internal
-    //     whenNotPaused
-    //     override
-    // {
-    //     super._beforeTokenTransfer(from, to, amount);
-    // }
-
-    // function deposit(uint256 assets, address receiver) public payable override returns (uint256) {
-    //     require(assets <= maxDeposit(receiver), "ERC4626: deposit more than max");
-
-    //     uint256 shares = previewDeposit(assets);
-    //     _deposit(_msgSender(), receiver, assets, shares);
-
-    //     return shares;
-    // }
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 }
